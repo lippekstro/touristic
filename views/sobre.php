ed75dcd9359482c15fa663ec6425d5ceba82587d
lippekstro <fellipecastro13@hotmail.com>
2023-01-19 21:43:52 -0300

    finalizando a parte sobre

--- views/sobre.php
+++ views/sobre.php
@@ -4,24 +4,42 @@ require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SE
 
 <div>
     <div class="tab">
-        <button class="tablinks active" onclick="openCity(event, 'London')">London</button>
-        <button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
-        <button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button>
+        <button class="tablinks active" onclick="openCity(event, 'London')">São Luis</button>
+        <button class="tablinks" onclick="openCity(event, 'Paris')">O Site</button>
+        <button class="tablinks" onclick="openCity(event, 'Tokyo')">Nossa Equipe</button>
     </div>
 
     <div id="London" class="tabcontent" style="display: block;">
-        <h3>London</h3>
-        <p>London is the capital city of England.</p>
+        <h3>São Luis</h3>
+        <p>
+            São Luís (frequentemente chamado de São Luís do Maranhão) é um município brasileiro e
+            a capital do estado do Maranhão.
+            É a única capital brasileira fundada por franceses,
+            no dia 8 de setembro de 1612, posteriormente invadida por holandeses
+            e por fim colonizada pelos portugueses.
+            Localiza-se na ilha de Upaon-Açu no Atlântico Sul,
+            entre as baías de São Marcos e São José de Ribamar, no Golfão Maranhense.
+            No ano de 1997 o centro histórico da cidade foi declarado
+            patrimônio cultural da humanidade pela UNESCO.
+        </p>
     </div>
 
     <div id="Paris" class="tabcontent" style="display: none;">
-        <h3>Paris</h3>
-        <p>Paris is the capital of France.</p>
+        <h3>O Site</h3>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Obcaecati reprehenderit totam eveniet aliquid et nostrum dolore consectetur,
+            officiis corporis vitae quas natus sunt voluptatum magnam officia velit deleniti nihil vero.
+        </p>
     </div>
 
     <div id="Tokyo" class="tabcontent" style="display: none;">
-        <h3>Tokyo</h3>
-        <p>Tokyo is the capital of Japan.</p>
+        <h3>Nossa Equipe</h3>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Obcaecati reprehenderit totam eveniet aliquid et nostrum dolore consectetur,
+            officiis corporis vitae quas natus sunt voluptatum magnam officia velit deleniti nihil vero.
+        </p>
     </div>
 </div>
 
